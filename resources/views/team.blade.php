6bad2aff6cb3180e7f0bab9c84f4788cdc9e2378
Ages <agesmugnia@gmail.com>
2021-04-18 22:10:03 +0700

    button

--- resources/views/team.blade.php
+++ resources/views/team.blade.php
@@ -9,8 +9,8 @@
             <h2 class="hue_blue">{{__("Our Team")}}</h2>
             <p class="hue_black">{{__("Meet our talented team members who shine when it comes to what they do the best")}}</p>
         </div>
-        <div class="members-slider mb-4">
-            <div class="member-single autoplay text-center">
+        <div class="row">
+            <div class="col-md-4">
                 <div class="team_member_row">
                     <img src="{{asset('images/team_images/Davit_.png')}}" alt="Davit" class="img-fluid m-auto">
                     <h4 class="hue_black pt-3">{{__("Davit Grigoryan")}}</h4>
@@ -18,7 +18,7 @@
                     <div class="layer hue_aqua"></div>
                 </div>
             </div>
-            <div class="member-single autoplay text-center">
+            <div class="col-md-4">
                 <div class="team_member_row">
                     <img src="{{asset('images/team_images/Davit.png')}}" alt=" Davit" class="img-fluid m-auto">
                     <h4 class="hue_black pt-3">{{__("Davit Kartashyan")}}</h4>
@@ -26,7 +26,7 @@
                     <div class="layer hue_royal"></div>
                 </div>
             </div>
-            <div class="member-single autoplay text-center">
+            <div class="col-md-4">
                 <div class="team_member_row">
                     <img src="{{asset('images/team_images/Armen.png')}}" alt="Armen" class="img-fluid m-auto">
                     <h4 class="hue_black pt-3">{{__("Armen Grigoryan")}}</h4>
@@ -34,16 +34,8 @@
                     <div class="layer hue_aqua"></div>
                 </div>
             </div>
-            <div class="member-single autoplay text-center">
-                <div class="team_member_row">
-                    <img src="{{asset('images/team_images/Arpine.png')}}" alt="Arpine" class="img-fluid m-auto">
-                    <h4 class="hue_black pt-3">{{__("Arpine Aleksanyan")}}</h4>
-                    <p class="hue_black pb-3">{{__("Senior QA Engineer")}}</p>
-                    <div class="layer hue_royal"></div>
-                </div>
-            </div>
         </div>
-    </div>
+    </div><br>
     <div class="container" align="center">
         <div class="custom-btn">
             <a class="hue_bg_b hue_blue" href="{{ route('teams') }}">See All</a>
