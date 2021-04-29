0fca8aa233b05599f199b2704134c9a617e3845b
Ages <agesmugnia@gmail.com>
2021-04-30 02:17:44 +0700

    feedback

--- resources/views/partners.blade.php
+++ resources/views/partners.blade.php
@@ -8,6 +8,8 @@
         </div>
         <div class="logos_row" align="center">
             <div class="row">
+            <div class="col-xl-1 col-lg-1 col-md-2 col-4 d-flex align-items-center justify-content-center">
+            </div>
                 <div class="col-xl-2 col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
                     <div class="logos_col severalnines_col" onmouseover="hvr(this, 'in')" onmouseleave="hvr(this, 'out')">
                         <img src="images/logo_black/cloudpcr.png" data-col="g" class="img-responsive logo_imgs">
@@ -38,6 +40,12 @@
                         <img src="images/logo_colorfull/luseen.png" style="display:none" data-col="b" class="img-responsive logo_imgs">
                     </div>
                 </div>
+            </div>
+            <div class="row">
+            <div class="col-xl-2 col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+            </div>
+            <div class="col-xl-2 col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+            </div>
                 <div class="col-xl-2 col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
                     <div class="logos_col big_bang_col" onmouseover="hvr(this, 'in')" onmouseleave="hvr(this, 'out')">
                         <img src="images/logo_black/sgames.png" data-col="g" class="img-responsive logo_imgs">
@@ -49,6 +57,10 @@
                         <img src="images/logo_black/bimbim.png" data-col="g" class="img-responsive logo_imgs">
                         <img src="images/logo_colorfull/bimbim.png" style="display:none" data-col="b" class="img-responsive logo_imgs">
                     </div>
+                </div>
+                <div class="col-xl-2 col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+                </div>
+                <div class="col-xl-2 col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
                 </div>
             </div>
         </div>
