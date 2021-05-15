b283fbb7d3ef998cf03b732e354e59b1351d7b3a
Ages <agesmugnia@gmail.com>
2021-05-16 03:27:09 +0700

    fix icon

--- resources/views/banner.blade.php
+++ resources/views/banner.blade.php
@@ -4,7 +4,7 @@
             <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12">
                 <div class="productive_col">
                     <h1 class="hue_blue">
-                        <span class="we_are_txt">{{__("A Company Of ")}}</span>
+                        <span class="we_are_txt">{{__("A Company Of")}}</span>
                         <span class="hue_yellow tesvan_txt">{{__("Quality")}}</span>
                     </h1>
                     <p class="hue_blue">{{__("We are highly-productive quality assurance team that appreciates clean and elegant solutions to multifaceted real-world business problems.")}}
