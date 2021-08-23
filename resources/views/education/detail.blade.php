7ab04fc483d7a71294d0521b4369e21babdb48d2
Bagaskara Wisnu Gunawan <bagaswisnu07@gmail.com>
2021-08-23 17:30:40 +0700

    fix: contain to cover on "interesting for you" section

--- resources/views/education/detail.blade.php
+++ resources/views/education/detail.blade.php
@@ -189,7 +189,7 @@
                                 <div class="qa_txt_blocks">
                                     <div class="row">
                                         <div class="col-md-4">
-                                            <img class="bradius" style="height: 100%; object-fit: contain; object-position: left;" width="100%" src="{{ asset("uploads/images/educations/$b->id/$b->image") }}" />
+                                            <img class="bradius" style="height: 100%; object-fit: cover; object-position: left;" width="100%" src="{{ asset("uploads/images/educations/$b->id/$b->image") }}" />
                                         </div>
                                         <div class="col-md-8">
                                             <div class="qa_txt_single_blok">
